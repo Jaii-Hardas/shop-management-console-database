da910347c3dab9f607e7c1b61fa2ac7c04897995
Jaii-Hardas <jaihardas1515@gmail.com>
2023-11-02 23:47:35 +0530

    Intial commit

---
+++ src/productmanagement/Product.java
@@ -0,0 +1,10 @@
+package productmanagement;
+
+public class Product {
+	String name;
+	String id;
+	String category;
+	String quantity;
+	String price;
+
+}
---
+++ src/productmanagement/ProductOptions.java
@@ -0,0 +1,10 @@
+package productmanagement;
+
+public class ProductOptions {
+	public static final int Add_product=1;
+	public static final int Edit_product=2;
+	public static final int Search_product=3;
+	public static final int delete_product=4;
+	public static final int Quite=5;
+
+}
